ae107e787d7b2273b515730c5624e3a5e457d6a7
jeee <a_jie_email@qq.com>
2019-03-30 18:31:39 +0800

    upload picture function and send email automatically

--- backend_demo/app.py
+++ backend_demo/app.py
@@ -1,23 +1,29 @@
-from flask import Flask,render_template,url_for,request,redirect,g,session
+from flask import Flask,render_template,url_for,request,redirect,g,session,jsonify,make_response
 import config
+from werkzeug.utils import secure_filename
 import os
+import cv2
+import time
 from datetime import timedelta
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
 
 
 app = Flask(__name__)
-# app.config.from_object(config)
 
-app.config['SECRET_KEY'] = os.urandom(24)
+app.secret_key = os.urandom(24)
+app.permanent_session_lifetime = timedelta(days=7)
+app.send_file_max_age_default = timedelta(seconds=10)
 
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'JPG', 'PNG', 'bmp'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def home():
-    # username = session['username']
-    # if session['username'] != None:
-    # if username != None:
-    #     return redirect('/1/')
-    # else:
-        return redirect('/0/')
+    return redirect('/0/')
 
 @app.route('/<is_login>/')
 def home_page(is_login):
@@ -33,7 +39,6 @@ def login_page():
         password = request.form['password']
 
         session['username'] = username
-        app.config['PERMANENT_SESSION_TIME'] = timedelta(days=7)
         session.permanent = True
 
         # search in the database to check whether this user is valid
@@ -69,13 +74,88 @@ def failure_page():
 @app.route('/deatil/',methods=['GET','POST'])
 def detail_page():
     if request.method == 'POST':
-        altered_username = request.form['alteredname']
-        altered_password = request.form['alteredpassword']
-        altered_phone = request.form['alteredphone']
-        altered_email = request.form['alteredemail']
-        return render_template('successpage.html')
+        if request.form['alteredname'] != '':
+            altered_username = request.form['alteredname']
+        if request.form['alteredpassword'] != '':
+            altered_password = request.form['alteredpassword']
+        if request.form['alteredphone'] != '':
+            altered_phone = request.form['alteredphone']
+        if request.form['alteredemail'] != '':
+            altered_email = request.form['alteredemail']
+
+        user_image = request.files['user_icon']
+        if not (user_image and allowed_file(user_image.filename)):
+            # 返回一个Json文件格式错误
+            return jsonify({"error": 1001, "msg": "请检查上传的图片类型，仅限于png、PNG、jpg、JPG、bmp"})
+        user_input = request.form.get("name")
+        basepath = os.path.dirname(__file__)  # 当前文件所在路径
+        upload_path = os.path.join(basepath, 'static/user_images', secure_filename(user_image.filename))  # 注意：没有的文件夹一定要先创建，不然会提示没有该路径
+        # upload_path = os.path.join(basepath, 'static/images','test.jpg')  #注意：没有的文件夹一定要先创建，不然会提示没有该路径
+        user_image.save(upload_path)
+
+        # 使用Opencv转换一下图片格式和名称
+        img = cv2.imread(upload_path)
+        cv2.imwrite(os.path.join(basepath, 'static/user_images', 'test.jpg'), img)
+
+        return render_template('detail_withicon.html', user_icon=user_input, val1=time.time())
+
+        # return render_template('successpage.html')
     else:
         return render_template('detail.html')
+
+@app.route('/claimpage/',methods=['GET','POST'])
+def claim_page():
+    if request.method == 'POST':
+        luggage_image = request.files['luggage_icon']
+        if not (luggage_image and allowed_file(luggage_image.filename)):
+            # 返回一个Json文件格式错误
+            return jsonify({"error": 1001, "msg": "请检查上传的图片类型，仅限于png、PNG、jpg、JPG、bmp"})
+        luggage_input = request.form.get("name")
+        basepath = os.path.dirname(__file__)  # 当前文件所在路径
+        upload_path = os.path.join(basepath, 'static/luggage_images',secure_filename(luggage_image.filename))  # 注意：没有的文件夹一定要先创建，不然会提示没有该路径
+        luggage_image.save(upload_path)
+
+        # 使用Opencv转换一下图片格式和名称
+        img = cv2.imread(upload_path)
+        cv2.imwrite(os.path.join(basepath, 'static/luggage_images', 'test.jpg'), img)
+
+        description = request.form.get('description')
+
+        return render_template('claimpage_withimage.html', user_icon=luggage_input, val1=time.time())
+    else:
+        return render_template('claimpage.html')
+
+@app.route('/verify/',methods=['GET','POST'])
+def verify_email_page():
+    if request.method == 'POST':
+        user_email = request.form['email_address']
+        # 第三方 SMTP 服务
+        mail_host = "smtp.163.com"  # 设置服务器
+        mail_user = "jiege2016youxiang@163.com"  # 用户名
+        mail_pass = "ShouQuan2019"  # 口令
+
+        sender = 'HS company'
+        receivers = user_email  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+
+        message = MIMEText('这是你的验证码，请妥善保管：123456', 'plain', 'utf-8')
+        message['From'] = mail_user
+        message['To'] = receivers
+
+        subject = '一封验证码邮件'
+        message['Subject'] = Header(subject, 'utf-8')
+
+        try:
+            smtpObj = smtplib.SMTP()
+            smtpObj.connect(mail_host, 25)  # 25 为 SMTP 端口号
+            smtpObj.login(mail_user, mail_pass)
+            smtpObj.sendmail(sender, receivers, message.as_string())
+            print("邮件发送成功")
+        except smtplib.SMTPException as e:
+            print("Error: 无法发送邮件")
+            print(e)
+        return render_template('successpage.html')
+    else:
+        return render_template('verify_eamilpage.html')
 
 @app.route('/logout/')
 def logout_page():
