e858271944a3873763cba1a371d1479e5ba030e4
ted <lhy19980302@163.com>
2019-04-06 11:32:34 +0800

    change request

--- Project/www/model/component/insurance_operate.py
+++ Project/www/model/component/insurance_operate.py
@@ -9,7 +9,7 @@ def add_insurance(dict):
     'id,name[0],product_id[1],amount_of_money[2],status,flight_number[3]'
     'status are the one of set (Creating, using, out_date)'
     assert (search_username(dict['name']) is not None), "No such Insurance"
-    f = Insurance(name=dict['name'], product_id=dict['product_id'], amount_of_money=dict['amount_of_money'], flight_number=dict['flight_number'], status=dict['status'], date =datetime.datetime.now() )
+    f = Insurance(name=dict['name'], product_id=dict['product_id'], amount_of_money=dict['amount_of_money'], flight_number=dict['flight_number'], status=dict['status'], date =datetime.datetime.now(),pic=dict['pic'] )
     db.session.add(f)
     db.session.commit()
     return f.id
